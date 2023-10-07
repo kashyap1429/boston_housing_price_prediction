bd3372a4206f06faab061017d85263366e802113
kashyap1429 <kashyappatel9108@gmail.com>
2023-10-08 01:29:16 +0530

    Final

--- app.py
+++ app.py
@@ -1,38 +1,42 @@
 import pickle
-import json
-
-from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
-import pandas as pd
+import streamlit as st
 
-app=Flask(__name__)
-gradient_boosting_model=pickle.load(open('gradient_boosting_model.pkl', 'rb'))
-scaler=pickle.load(open('scaling.pkl', 'rb'))
+gradient_boosting_model = pickle.load(open('gradient_boosting_model.pkl', 'rb'))
+scaler = pickle.load(open('scaling.pkl', 'rb'))
 
-@app.route('/')
-def home():
-    return render_template('home.html')
+# st.set_page_config(
+#     page_title="Boston House Price Prediction",
+#     page_icon=" "
+# )
 
-@app.route('/predict_api', methods=['POST'])
-def predict_api():
-    data=request.json['data']
-    print(data)
-    print(np.array(list(data.values())).reshape(1,-1))
-    new_data=scaler.transform(np.array(list(data.values())).reshape(1,-1))
-    output=gradient_boosting_model.predict(new_data)
-    print(output[0])
-    return jsonify(output[0])
+st.title("Boston House Price Prediction 🏡")
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    data=[float(x) for x in request.form.values()]
-    final_input=scaler.transform(np.array(data).reshape(1,-1))
-    print(final_input)
-    output=gradient_boosting_model.predict(final_input)
-    return render_template('home.html', prediction_text="The predicted house price is {}".format(output))
+CRIM = st.number_input('CRIM')
+ZN = st.number_input('ZN')
+INDUS = st.number_input('INDUS')
+CHAS = st.number_input('CHAS')
+NOX = st.number_input('NOX')
+RM = st.number_input('RM')
+AGE = st.number_input('AGE')
+DIS = st.number_input('DIS')
+RAD = st.number_input('RAD')
+TAX = st.number_input('TAX')
+PTRATIO = st.number_input('PTRATIO')
+B = st.number_input('B')
+LSTAT = st.number_input('LSTAT')
+
+if st.button('Predict'):
+    # Preprocess the input data
+    input_data = np.array([CRIM, ZN, INDUS, CHAS, NOX, RM, AGE, DIS, RAD, TAX, PTRATIO, B, LSTAT]).reshape(1, -1)
+    input_data_scaled = scaler.transform(input_data)
     
+    # Make a prediction
+    prediction = gradient_boosting_model.predict(input_data_scaled)
+    
+    # Display the prediction
+    st.write(f'The predicted house price is: {prediction[0]}')
 
-
-
-if __name__=='__main__':
-    app.run(debug=True)
+# Run the Streamlit app
+if __name__ == '__main__':
+    st.run()
